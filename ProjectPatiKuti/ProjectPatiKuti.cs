e244b583a4b8715a1e5815e70491bc86db6e038d
Niklaspoo <nipe.pollanen@gmail.com>
2024-11-14 13:11:45 +0200

    liikunta on tärkeää

--- ProjectPatiKuti/ProjectPatiKuti.cs
+++ ProjectPatiKuti/ProjectPatiKuti.cs
@@ -14,9 +14,48 @@ namespace ProjectPatiKuti
     /// </summary>
     public class ProjectPatiKuti : PhysicsGame
     {
+        private Vector pelaajanNopeus = new Vector(0, 0); 
+        private PhysicsObject pelaaja; 
         public override void Begin()
         {
             Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Exit PPK");
+            luoPelaaja();
+        }
+
+        public void luoPelaaja()
+        {
+            pelaaja = new PhysicsObject(40, 40); 
+            pelaaja.Shape = Shape.Circle;
+            pelaaja.Color = Color.Red;
+            pelaaja.Velocity = pelaajanNopeus;
+            pelaaja.Position = new Vector(0, 0);
+            Add(pelaaja);
+            lisaaOhjaimet();
+        }
+        private void lisaaOhjaimet()
+        {
+            Keyboard.Listen(Key.W, ButtonState.Pressed, liiku, "move", new Vector(000, 500));
+            Keyboard.Listen(Key.W, ButtonState.Released, liiku, "move", new Vector(0, -500));
+            Keyboard.Listen(Key.D, ButtonState.Pressed, liiku, "move", new Vector(500, 0));
+            Keyboard.Listen(Key.D, ButtonState.Released, liiku, "move", new Vector(-500, 0));
+            Keyboard.Listen(Key.S, ButtonState.Pressed, liiku, "move", new Vector(0, -500));
+            Keyboard.Listen(Key.S, ButtonState.Released, liiku, "move", new Vector(0, 500));
+            Keyboard.Listen(Key.A, ButtonState.Pressed, liiku, "move", new Vector(-500, 0));
+            Keyboard.Listen(Key.A, ButtonState.Released, liiku, "move", new Vector(500, 0));
+        }
+
+        private void liiku(Vector nopeus)
+        {
+            if ((Math.Abs(pelaajanNopeus.X) == 250) && (Math.Abs(pelaajanNopeus.Y) == 250))
+            {
+                pelaajanNopeus = pelaajanNopeus * 2;
+            }
+            pelaajanNopeus += nopeus;
+            if ((Math.Abs(pelaajanNopeus.X) == 500) && (Math.Abs(pelaajanNopeus.Y) == 500))
+            {
+                pelaajanNopeus = pelaajanNopeus / 2;
+            }
+            pelaaja.Velocity = pelaajanNopeus;
         }
     }
 
